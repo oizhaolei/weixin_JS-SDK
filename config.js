5db21804720fd2214ba730f8d0a2b6d33f94025e
zhaolei <oizhaolei@gmail.com>
2015-12-31 00:24:12 -0500

    config

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = function() {
     // 输入你的配置
     return {
-        appId: '',
-        appSecret: '',
-        appToken: 'SHANG'
+        appId: 'wx99b8690b0397ad16',
+        appSecret: 'd9c686baa3a90b84c2d08a71a2fbf846',
+        appToken: '62064bef9df47ed285cb428c4c'
     };
 };
