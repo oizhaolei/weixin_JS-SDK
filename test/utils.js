8bdeeb4f3466fba1df1a9abbeb859398983f8f94
oizhaolei <oizhaolei@gmail.com>
2016-01-14 19:43:25 +0800

    去掉signature模块

--- test/utils.js
+++ test/utils.js
@@ -2,19 +2,8 @@ var assert = require('assert');
 var config = require('../config.json');
 
 var logger = require('log4js').getLogger('test/utils.js');
-var signature = require('../signature');
 
 describe('utils', function () {
-
-  it('signature', function (done) {
-
-    var url = '';
-    signature.getSignature(url, function(error, result) {
-      logger.info(error);
-      logger.info(result);
-      done();
-    });
-  });
 
   it('util', function (done) {
     var now = new Date().getTime();
